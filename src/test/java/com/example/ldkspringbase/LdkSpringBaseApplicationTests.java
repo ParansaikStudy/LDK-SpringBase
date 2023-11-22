74e09a239d02db3667caaa0e6f42e2050e443304
dbzoseh2rl <dbzoseh2rl123@naver.com>
2023-11-23 04:40:19 +0900

    Initial commit

---
+++ src/test/java/com/example/ldkspringbase/LdkSpringBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ldkspringbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LdkSpringBaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
