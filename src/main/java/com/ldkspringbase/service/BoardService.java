34173f42461694df2ac6b05860d8fcc22e602a22
dbzoseh2rl <dbzoseh2rl123@naver.com>
2023-12-23 01:05:56 +0900

    feat: u-d-implementation

--- src/main/java/com/ldkspringbase/service/BoardService.java
+++ src/main/java/com/ldkspringbase/service/BoardService.java
@@ -31,5 +31,36 @@ public class BoardService {
             throw new RuntimeException("보드 생성에 실패하였습니다.", e);
         }
     }
+    public boolean updateBoard(int id, BoardEntity board) {
+        try {
+            BoardEntity existingBoard = boardMapper.getBoardById(id);
+            if (existingBoard != null) {
+                board.setId(id);
+                boardMapper.updateBoard(board);
+                return true;
+            } else {
+                // Board not found, update failed
+                return false;
+            }
+        } catch (Exception e) {
+            // 실패 시 예외 처리
+            throw new RuntimeException("보드 업데이트에 실패하였습니다.", e);
+        }
+    }
 
+    public boolean deleteBoard(int id) {
+        try {
+            BoardEntity existingBoard = boardMapper.getBoardById(id);
+            if (existingBoard != null) {
+                boardMapper.deleteBoard(id);
+                return true;
+            } else {
+                // Board not found, delete failed
+                return false;
+            }
+        } catch (Exception e) {
+            // 실패 시 예외 처리
+            throw new RuntimeException("보드 삭제에 실패하였습니다.", e);
+        }
+    }
 }
